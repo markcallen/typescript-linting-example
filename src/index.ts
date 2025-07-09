b4a43b64c0bf72eada8a0c061d844dcee5147f67
Mark C Allen <mark@markcallen.com>
2025-07-09 13:24:44 -0700

    Ran prettier and fixed linter warnings

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-console.log('hello world!!');
+console.log('hello world!!'); // eslint-disable-line no-console
 
 // Keep the process alive until Ctrl-C
 process.stdin.resume();
 
 // Optional: Handle graceful shutdown on Ctrl-C
 process.on('SIGINT', () => {
-  console.log('\nGoodbye!');
+  console.log('\nGoodbye!'); // eslint-disable-line no-console
   process.exit();
 });
